e5d1ea6ac1ad13d36e42a0866ef39f7ebe03b410
zabefofoon <zabefofoon@naver.com>
2024-11-29 10:05:48 +0900

    chore: .vscode/setting.json, stylelint 적용, normalize 추가, icons 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,18 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint"],
+    modules: ["@nuxt/eslint", "@nuxtjs/stylelint-module"],
     eslint: {
         checker: true,
+    },
+    css: ["~/assets/styles/main.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler", // or "modern"
+                },
+            },
+        },
     },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>index page</div>
+    <div class="index">index page</div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+    .index {
+        color: black;
+    }
+</style>
