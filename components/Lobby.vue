f483e23a62f5495b0353f4b5afdea79eda907351
zabefofoon <zabefofoon@gmail.com>
2025-03-31 00:37:38 +0900

    feat 간단한 로비 구현

--- components/Lobby.vue
+++ components/Lobby.vue
@@ -1,16 +1,56 @@
 <template>
   <div class="w-screen h-screen | flex items-center justify-center | bg-black">
     <div class="content | relative | aspect-video max-w-full max-h-full | bg-white">
-      <button @click="emit('next')">Next</button>
-      <button @click="fullScreen">full</button>
-      <VirtualJoystick class="absolute bottom-[12px] left-[12px]" />
+      <!-- <button @click="emit('next')">Next</button> -->
+
+      <main
+        class="relative | w-full h-full | flex flex-col justify-center items-center | bg-purple-950">
+        <button
+          class="absolute top-[1cqw] right-[1cqw] | border rounded-full p-1 | bg-white"
+          @click="fullScreen">
+          <i class="icon icon-fullscreen | block text-black text-[1.5cqw]"></i>
+        </button>
+        <img
+          class="absolute right-0 bottom-0 w-[30cqw] | select-none pointer-events-none"
+          src="~/assets/images/main.png" />
+        <h1
+          class="gasoek-one-regular | relative z-[1] | leading-none text-center text-[5cqw] font-bold text-white [text-shadow:_.5cqw_.5cqw_0_black] shadow-black">
+          <span class="text-red-500">N</span>
+          <span class="text-orange-500">Y</span>
+          <span class="text-blue-500">L</span>
+          <span class="text-green-500">O</span>
+          <sapn class="text-violet-500">N</sapn> MASK<br />
+          RANDOM DEFENCE
+        </h1>
+        <div class="flex flex-col | mt-[2cqw] | font-bold text-[2cqw] text-white">
+          <button
+            class="button | flex items-center | relative"
+            @click="emit('next', 'game')">
+            <i class="icon icon-chevron-right | absolute left-0 -translate-x-full | hidden"></i>
+            Game Start
+          </button>
+          <button
+            class="button | flex items-center | relative"
+            @click="emit('next', 'collection')">
+            <i class="icon icon-chevron-right | absolute left-0 -translate-x-full | hidden"></i>
+            Collection
+          </button>
+          <button
+            class="button | flex items-center | relative"
+            @click="emit('next', 'setting')">
+            <i class="icon icon-chevron-right | absolute left-0 -translate-x-full | hidden"></i>
+            Setting
+          </button>
+        </div>
+      </main>
+      <VirtualJoystick class="md:hidden | absolute bottom-[12px] left-[12px]" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const emit = defineEmits<{
-  (e: "next"): void
+  (e: "next", scene: "game" | "collection" | "setting"): void
 }>()
 
 const fullScreen = async () => {
@@ -44,6 +84,14 @@ const fullScreen = async () => {
   @media (aspect-ratio <= 16 / 9) {
     width: 100%;
     height: auto;
+  }
+}
+
+.button {
+  &:hover {
+    .icon {
+      display: block;
+    }
   }
 }
 </style>
