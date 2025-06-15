0428da15e987ed7ffd35ea1c4fb353e06f1f9cc0
zabefofoon <zabefofoon@gmail.com>
2025-06-15 22:01:27 +0900

    feat: 스크롤 안되도록 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -129,5 +129,13 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/randi/",
     buildAssetsDir: "assets",
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;",
+        },
+      ],
+    },
   },
 })
